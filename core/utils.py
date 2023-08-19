152cb60be37d1b50b3fe39fe1fde83460b19a2f4
orlovskiy-artem <artem.orlovsky@gmail.com>
2023-08-20 00:12:08 +0300

    changed globals to args in create_knowledge_v func

--- core/utils.py
+++ core/utils.py
@@ -17,11 +17,11 @@ def load_texts(dirpath):
     return [doc.page_content for doc in documents]
 
 
-def create_knowledge_vectordb(dirpath, embedder):
+def create_knowledge_vectordb(dirpath, embedder, chroma_client, chroma_persist_directory):
     collection_names = []
     for folder in glob.glob(os.path.join(dirpath, "*")):
         documents = load_documents(folder)
         db = Chroma.from_documents(documents=documents, embedding=embedder, collection_name=os.path.basename(folder),
-                                   client=chroma_client, persist_directory=CHROMA_PERSIST_DIRECTORY)
+                                   client=chroma_client, persist_directory=chroma_persist_directory)
         collection_names.append(db._collection.name)
     return collection_names
